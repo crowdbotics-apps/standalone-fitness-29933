6846d33a8e8c1c3e6ea0f64c73fd1e7052d95888
Crowdbotics <team@crowdbotics.com>
2021-08-18 23:02:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "standalone_fitness_29933Identifier",
+  url: "https://standalone_fitness_29933Identifier.botics.co",
+  api: "https://standalone_fitness_29933Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
